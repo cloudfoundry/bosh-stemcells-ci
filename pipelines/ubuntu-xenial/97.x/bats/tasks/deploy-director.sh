95dfd8f9741fcca95ee91964bab525a72b61a5f8
Luan Santos <lsantos@pivotal.io>
2019-02-13 16:04:31 -0800

    Bring xenial/97.x pipeline
    * We also added 170.x, 250.a, and master. Please ignore these.
    
    [#163964067]
    
    Co-authored-by: Aakash Shah <ashah@pivotal.io>

---
+++ pipelines/ubuntu-xenial/97.x/bats/tasks/deploy-director.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+source /etc/profile.d/chruby.sh
+chruby ruby
+
+set -e
+
+function cp_artifacts {
+  mv $HOME/.bosh director-state/
+  cp director.yml director-creds.yml director-state.json director-state/
+}
+
+trap cp_artifacts EXIT
+
+: ${BAT_INFRASTRUCTURE:?}
+
+mv bosh-cli/bosh-cli-* /usr/local/bin/bosh-cli
+chmod +x /usr/local/bin/bosh-cli
+
+bosh-cli interpolate bosh-deployment/bosh.yml \
+  -o bosh-deployment/$BAT_INFRASTRUCTURE/cpi.yml \
+  -o bosh-deployment/$BAT_INFRASTRUCTURE/resource-pool.yml \
+  -o bosh-deployment/misc/powerdns.yml \
+  -o bosh-deployment/jumpbox-user.yml \
+  -o bosh-stemcells-ci/pipelines/ubuntu-xenial/97.x/bats/ops/remove-health-monitor.yml \
+  -o bosh-stemcells-ci/pipelines/ubuntu-xenial/97.x/bats/ops/use-candidate-stemcell.yml \
+  -v dns_recursor_ip=8.8.8.8 \
+  -v director_name=bats-director \
+  -v local_candidate_stemcell=$(realpath stemcell/*.tgz) \
+  --vars-file <( bosh-stemcells-ci/pipelines/ubuntu-xenial/97.x/bats/iaas/$BAT_INFRASTRUCTURE/director-vars ) \
+  > director.yml
+
+bosh-cli create-env \
+  --state director-state.json \
+  --vars-store director-creds.yml \
+  director.yml
---
+++ pipelines/ubuntu-xenial/configure-aggregated-pipeline.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dir=$(dirname $0)
+
+fly -t production set-pipeline \
+  -p "bosh:stemcells:ubuntu-xenial" \
+  -c <(
+    bosh interpolate \
+      -o <( bosh int -v group=master -v branch=master -v initial_version=0.0.0  -v bump_version=major -v bosh_agent_version='"*"' "$dir/master/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int "$dir/master/ubuntu-xenial/pipeline-master-ops.yml" ) \
+      -o <( bosh int -v group=97.x -v branch=ubuntu-xenial/97.x -v initial_version=97.0.0 -v bump_version=minor "$dir/97.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int -v group=97.x -v branch=ubuntu-xenial/97.x -v initial_version=97.0.0 -v bump_version=minor "$dir/97.x/ubuntu-xenial/pipeline-branch-ops.yml" ) \
+      -o <( bosh int -v group=170.x -v branch=ubuntu-xenial/170.x -v initial_version=170.0.0 -v bump_version=minor "$dir/170.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int -v group=170.x -v branch=ubuntu-xenial/170.x -v initial_version=170.0.0 -v bump_version=minor "$dir/170.x/ubuntu-xenial/pipeline-branch-ops.yml" ) \
+      -o <( bosh int -v group=250.x -v branch=ubuntu-xenial/250.x -v initial_version=250.0.0 -v bump_version=minor -v bosh_agent_version='"2.193.*"' "$dir/250.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int -v group=250.x -v branch=ubuntu-xenial/250.x -v initial_version=250.0.0 -v bump_version=minor "$dir/250.x/ubuntu-xenial/pipeline-branch-ops.yml" ) \
+      "$dir/master/ubuntu-xenial/pipeline-base.yml"
+  ) \
+  -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
+  -l <( lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
+  -l <( lpass show --notes "bats-concourse-pool:vsphere secrets" ) \
+  -l <(lpass show --notes "stemcell-reminder-bot")
