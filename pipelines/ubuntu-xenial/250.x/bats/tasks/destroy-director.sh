5f417b1109d42a5ab5e56bcdb5075b99770e9e06
Joshua Aresty <joshua.aresty@emc.com>
2019-04-19 11:06:20 -0700

    Use bosh-cli via input
    [#165460641, #165457872]
    
    Co-authored-by: Nicholas Calugar <ncalugar@pivotal.io>

--- pipelines/ubuntu-xenial/250.x/bats/tasks/destroy-director.sh
+++ pipelines/ubuntu-xenial/250.x/bats/tasks/destroy-director.sh
@@ -2,9 +2,13 @@
 
 set -eu
 
-mv director-state/.bosh $HOME/
+mv director-state/.bosh "$HOME"/
 
-state_path() { bosh-cli int director-state/director.yml --path="$1" ; }
+bosh_cli=$(realpath bosh-cli/bosh-cli-*)
+export bosh_cli
+chmod +x "$bosh_cli"
+
+state_path() { $bosh_cli int director-state/director.yml --path="$1" ; }
 
 function get_bosh_environment {
   if [[ -z $(state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null) ]]; then
@@ -14,13 +18,16 @@ function get_bosh_environment {
   fi
 }
 
-export BOSH_ENVIRONMENT=`get_bosh_environment`
-export BOSH_CA_CERT=`bosh-cli int director-state/director-creds.yml --path /director_ssl/ca`
+BOSH_ENVIRONMENT=$(get_bosh_environment)
+export BOSH_ENVIRONMENT
+BOSH_CA_CERT=$("$bosh_cli" int director-state/director-creds.yml --path /director_ssl/ca)
+export BOSH_CA_CERT
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`bosh-cli int director-state/director-creds.yml --path /admin_password`
+BOSH_CLIENT_SECRET=$("$bosh_cli" int director-state/director-creds.yml --path /admin_password)
+export BOSH_CLIENT_SECRET
 
 set +e
 
-bosh-cli deployments --column name | xargs -n1 -I % bosh-cli -n -d % delete-deployment
-bosh-cli clean-up -n --all
-bosh-cli delete-env -n director-state/director.yml -l director-state/director-creds.yml
+"$bosh_cli" deployments --column name | xargs -n1 -I % "$bosh_cli" -n -d % delete-deployment
+"$bosh_cli" clean-up -n --all
+"$bosh_cli" delete-env -n director-state/director.yml -l director-state/director-creds.yml
