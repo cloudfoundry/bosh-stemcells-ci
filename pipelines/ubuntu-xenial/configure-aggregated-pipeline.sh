20e2a1f1076efbe340be7cfec70b065a6ed215de
Aakash Shah <ashah@pivotal.io>
2019-06-20 11:27:14 -0700

    De-dupe pipeline-base-ops over {250,315,master}
    * Could not de-dupe 97 & 170. Those pipelines DO NOT have
    auto bumping of the bosh-agent. It appears we made this
    significantly easier in the bosh-linux-stemcell-builder in
    250 and onward.

--- pipelines/ubuntu-xenial/configure-aggregated-pipeline.sh
+++ pipelines/ubuntu-xenial/configure-aggregated-pipeline.sh
@@ -8,15 +8,15 @@ fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
   -c <(
     bosh interpolate \
-      -o <( bosh int -v group=master -v branch=master -v initial_version=0.0.0  -v bump_version=major -v bosh_agent_version='"*"' "$dir/master/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int -v group=master -v branch=master -v initial_version=0.0.0  -v bump_version=major -v bosh_agent_version='"*"' "$dir/pipeline-base-ops.yml" ) \
       -o <( bosh int "$dir/master/ubuntu-xenial/pipeline-master-ops.yml" ) \
       -o <( bosh int -v group=97.x -v branch=ubuntu-xenial/97.x -v initial_version=97.0.0 -v bump_version=minor "$dir/97.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
       -o <( bosh int -v group=97.x -v branch=ubuntu-xenial/97.x -v initial_version=97.0.0 -v bump_version=minor "$dir/pipeline-branch-ops.yml" ) \
       -o <( bosh int -v group=170.x -v branch=ubuntu-xenial/170.x -v initial_version=170.0.0 -v bump_version=minor "$dir/170.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
       -o <( bosh int -v group=170.x -v branch=ubuntu-xenial/170.x -v initial_version=170.0.0 -v bump_version=minor "$dir/pipeline-branch-ops.yml" ) \
-      -o <( bosh int -v group=250.x -v branch=ubuntu-xenial/250.x -v initial_version=250.0.0 -v bump_version=minor -v bosh_agent_version='"2.193.*"' "$dir/250.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int -v group=250.x -v branch=ubuntu-xenial/250.x -v initial_version=250.0.0 -v bump_version=minor -v bosh_agent_version='"2.193.*"' "$dir/pipeline-base-ops.yml" ) \
       -o <( bosh int -v group=250.x -v branch=ubuntu-xenial/250.x -v initial_version=250.0.0 -v bump_version=minor "$dir/pipeline-branch-ops.yml" ) \
-      -o <( bosh int -v group=315.x -v branch=ubuntu-xenial/315.x -v initial_version=315.0.0 -v bump_version=minor -v bosh_agent_version='"2.215.*"' "$dir/315.x/ubuntu-xenial/pipeline-base-ops.yml" ) \
+      -o <( bosh int -v group=315.x -v branch=ubuntu-xenial/315.x -v initial_version=315.0.0 -v bump_version=minor -v bosh_agent_version='"2.215.*"' "$dir/pipeline-base-ops.yml" ) \
       -o <( bosh int -v group=315.x -v branch=ubuntu-xenial/315.x -v initial_version=315.0.0 -v bump_version=minor "$dir/pipeline-branch-ops.yml" ) \
       "$dir/master/ubuntu-xenial/pipeline-base.yml"
   ) \
