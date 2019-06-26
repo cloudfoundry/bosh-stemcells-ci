839200f893de50458a7110b6473c0f019a3f05a5
Joshua Aresty <joshua.aresty@emc.com>
2019-06-26 14:54:15 -0700

    Create the usn_json file if it does not exist
    [#166618973]
    
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- tasks/write-usn-message.sh
+++ tasks/write-usn-message.sh
@@ -3,5 +3,6 @@
 set -euo pipefail
 
 usn_json="${PWD}/usn-source/usn.json"
+touch $usn_json
 
 jq -s --slurpfile new_usn $usn_json '. + $new_usn | unique | .[]' > usn-log-out/usn.json < usn-log-in/usn-log.json
