3edcf1f1aabff45d7610e3d23965e08d73e47a70
Joshua Aresty <joshua.aresty@emc.com>
2019-06-26 14:57:35 -0700

    Make the task input if it does not exist
    [#166618973]
    
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- tasks/write-usn-message.sh
+++ tasks/write-usn-message.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 usn_json="${PWD}/usn-source/usn.json"
+mkdir -p usn-log-in
 touch usn-log-in/usn-log.json
 
 jq -s --slurpfile new_usn $usn_json '. + $new_usn | unique | .[]' > usn-log-out/usn.json < usn-log-in/usn-log.json
