56dfed28dafe0cee0871ab31f8048f7b93e733c1
Joshua Aresty <joshua.aresty@emc.com>
2019-06-26 14:37:47 -0700

    Write usn info as a log instead of commits
    [#166618973]
    
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- tasks/write-usn-message.sh
+++ tasks/write-usn-message.sh
@@ -4,18 +4,4 @@ set -euo pipefail
 
 usn_json="${PWD}/usn-source/usn.json"
 
-commit_body=$(cat <<EOF
-url: $(jq -r .url "${usn_json}")
-priorities: $(jq -r '.priorities | join(",")' "${usn_json}")
-description: $(jq -r .description "${usn_json}")
-cves:
-  $(jq -r '.cves[] | "* \(.)"' "${usn_json}")
-EOF
-)
-
-pushd bosh-linux-stemcell-builder
-  git add -A
-  git config --global user.email "ci@localhost"
-  git config --global user.name "CI Bot"
-  git commit --allow-empty -m "$(jq -r .title "${usn_json}")" -m "${commit_body}"
-popd
+jq -s --slurpfile new_usn $usn_json '. + $new_usn | unique | .[]' > usn-log-out/usn.json < usn-log-in/usn-log.json
