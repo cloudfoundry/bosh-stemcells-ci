fe9899f30b03a46f108eb3094041d95b7d193c65
Joshua Aresty <joshua.aresty@emc.com>
2019-06-28 10:32:45 -0700

    Fixup spacing in notes

--- pipelines/ubuntu-bionic/configure.sh
+++ pipelines/ubuntu-bionic/configure.sh
@@ -15,7 +15,7 @@ dir="$(dirname "$0")"
 fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-bionic" \
   -c "$dir/pipeline.yml" \
-  -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
-  -l <( lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
-  -l <( lpass show --notes "bats-concourse-pool:vsphere secrets" ) \
+  -l <(lpass show --notes "concourse:production pipeline:os-images") \
+  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells") \
+  -l <(lpass show --notes "bats-concourse-pool:vsphere secrets") \
   -l <(lpass show --notes "stemcell-reminder-bot")
