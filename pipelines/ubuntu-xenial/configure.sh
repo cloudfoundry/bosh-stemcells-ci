f2bd23d7efaf65b2252d08645e225af9d6395df8
Aakash Shah <ashah@pivotal.io>
2019-09-26 12:15:07 -0700

    Manually merge ytt files
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>

--- pipelines/ubuntu-xenial/configure.sh
+++ pipelines/ubuntu-xenial/configure.sh
@@ -1,37 +1,20 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -e
 
 until lpass status;do
   LPASS_DISABLE_PINENTRY=1 lpass ls a
+  sleep 1
 done
 
-until fly -t production status;do
-  fly -t production login
+until fly -t pcf status;do
+  fly -t pcf login
+  sleep 1
 done
 
-dir=$(dirname $0)
-
-interpolated=$(mktemp)
-
-bosh interpolate \
-  -o <( bosh int -v group=master -v branch=master -v initial_version=0.0.0  -v bump_version=major -v bosh_agent_version='"*"' "$dir/pipeline-base-ops.yml" ) \
-  -o "$dir/pipeline-master-ops.yml" \
-  "${dir}/pipeline-base.yml" > "${interpolated}"
-
-while read -r xenial_line bosh_agent_version; do
-  bosh interpolate \
-    -o <(bosh int -v group="${xenial_line}.x"  -v branch="ubuntu-xenial/${xenial_line}.x"  -v initial_version="${xenial_line}.0.0" -v bump_version=minor -v bosh_agent_version="${bosh_agent_version}.*" "$dir/pipeline-base-ops.yml") \
-    -o <(bosh int -v group="${xenial_line}.x"  -v branch="ubuntu-xenial/${xenial_line}.x"  -v initial_version="${xenial_line}.0.0" -v bump_version=minor "$dir/pipeline-branch-ops.yml") \
-    "$interpolated" > "${interpolated}.next"
-
-  mv "${interpolated}.next" "$interpolated"
-done < "${dir}/stemcell-lines.txt"
-
-bosh interpolate \
-  -o "${dir}/../../ops-files/97.x-delete-alicloud-build-ops.yml" \
-  "$interpolated" > "${interpolated}.next"
-mv "${interpolated}.next" "$interpolated"
+pipeline_config=$(mktemp)
+ytt -f "$(dirname $0)" > $pipeline_config
+# ytt -f "$(dirname $0)"
 
 fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
@@ -39,4 +22,5 @@ fly -t production set-pipeline \
   -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
   -l <(lpass show --notes "bats-concourse-pool:vsphere secrets" ) \
   -l <(lpass show --notes "stemcell-reminder-bot") \
-  -c "$interpolated"
+  -c "$pipeline_config"
+
