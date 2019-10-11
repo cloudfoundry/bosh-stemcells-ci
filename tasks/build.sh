4535e75ab685e466d40f25c4bf8355f026dc8812
Joshua Aresty <joshua.aresty@emc.com>
2019-10-11 11:24:57 -0700

    Output candidate-build-number
    The metalink repository resource requires a build number, and we were
    stripping out the minor and patch versions in the build task before.
    This allows the metalink-repository-resource to behave the same.
    
    [#167075370]
    
    Co-authored-by: Conor Nosal <cnosal@pivotal.io>

--- tasks/build.sh
+++ tasks/build.sh
@@ -87,6 +87,7 @@ SUDO
 stemcell_name="bosh-stemcell-$CANDIDATE_BUILD_NUMBER-$IAAS-$HYPERVISOR-$OS_NAME-$OS_VERSION-go_agent"
 meta4_path=$TASK_DIR/stemcells-index-output/dev/$OS_NAME-$OS_VERSION/$CANDIDATE_BUILD_NUMBER/$IAAS-$HYPERVISOR-go_agent.meta4
 
+echo $CANDIDATE_BUILD_NUMBER > candidate-build-number/number
 mkdir -p "$( dirname "$meta4_path" )"
 rm -f "$meta4_path"
 meta4 create --metalink="$meta4_path"
