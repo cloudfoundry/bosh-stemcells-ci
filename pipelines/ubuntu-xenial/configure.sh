558e0f6ed1c8f0a3aee0fa5a2be2017d46cc5fa7
Joshua Aresty <joshua.aresty@emc.com>
2019-10-15 11:10:52 -0700

    Bring bosh-agent pipeline into stemcell pipeline
    [#167075370](https://www.pivotaltracker.com/story/show/167075370)

--- pipelines/ubuntu-xenial/configure.sh
+++ pipelines/ubuntu-xenial/configure.sh
@@ -20,6 +20,7 @@ fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
   -l <(lpass show --notes "concourse:production pipeline:os-images" ) \
   -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
+  -l <(lpass show --notes "bosh-agent concourse secrets" ) \
   -l <(lpass show --notes "bats-concourse-pool:vsphere secrets" ) \
   -l <(lpass show --notes "stemcell-reminder-bot") \
   -c "$pipeline_config"
