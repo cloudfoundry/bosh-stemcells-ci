17c17edafd2cd26b25ea5c56bd3db376f788271b
Aakash Shah <ashah@pivotal.io>
2019-12-11 11:41:13 -0800

    Remove centos-master pipeline

--- pipelines/centos-7/configure-aggregated-pipeline.sh
+++ pipelines/centos-7/configure-aggregated-pipeline.sh
@@ -16,10 +16,9 @@ fly -t production set-pipeline \
   -p "bosh:stemcells:centos-7" \
   -c <(
     bosh interpolate \
-      -o <( bosh int -v group=master -v branch=master -v initial_version="3728.0.0" -v bump_version=major "$dir/master/centos-7/pipeline-base-ops.yml" ) \
       -o <( bosh int -v group=3763.x -v branch=centos-7/3763.x -v initial_version="3763.0.0" -v bump_version=minor "$dir/3763.x/centos-7/pipeline-base-ops.yml" ) \
       -o <( bosh int -v group=3763.x -v branch=centos-7/3763.x -v initial_version="3763.0.0" -v bump_version=minor "$dir/3763.x/centos-7/pipeline-branch-ops.yml" ) \
-      "$dir/master/centos-7/pipeline-base.yml"
+      "$dir/pipeline-base.yml"
   ) \
   -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
   -l <( lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
