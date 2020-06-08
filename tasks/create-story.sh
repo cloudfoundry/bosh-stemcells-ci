697819be878b8e8b63d0b211ac51f99b99f09ced
mrosecrance <mrosecrance@pivotal.io>
2020-06-08 14:49:29 -0700

    Add link to instructions in story
    Makes it a bit more discoverable

--- tasks/create-story.sh
+++ tasks/create-story.sh
@@ -20,7 +20,7 @@ curl -X POST "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/st
   "current_state" : "unstarted",
   "estimate" : 0,
   "name" : "_$(date +%Y-%0m-%0d)_ - stemcell ${DESCRIPTION} [**$BRANCH**]",
-  "description" : "It's time to build a new version of stemcells which include the latest upstream vulnerability fixes. The pipeline should have automatically triggered when this story was created to kick off that process.\\n\\nEnsure the OS image and subsequent stemcell builds finished successfully.\\n\\n**Acceptance Criteria**\\n\\n* A new version of the stemcell can be published from the pipeline.",
+  "description" : "It's time to build a new version of stemcells which include the latest upstream vulnerability fixes. The pipeline should have automatically triggered when this story was created to kick off that process. Detailed instructions on what to do to make a patch lives [here](https://github.com/pivotal-cf/bosh-team/blob/master/stemcells/flow-of-stemcell-pipelines.md#cutting-a-new-stemcell-patch)\\n\\nEnsure the OS image and subsequent stemcell builds finished successfully.\\n\\n**Acceptance Criteria**\\n\\n* A new version of the stemcell can be published from the pipeline.",
   "story_type" : "feature",
   "labels" : ["$BRANCH", "stemcell", "sec"],
   "before_id" : $FIRST_UNSTARTED_STORY
