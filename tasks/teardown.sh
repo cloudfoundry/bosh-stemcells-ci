2271c2fde2461b135981fccd91234657adfdd5b5
Maya Rosecrance <mrosecrance@vmware.com>
2020-08-13 12:19:27 -0700

    Director teardown should delete deployments+blobs first
    [#172917983](https://www.pivotaltracker.com/story/show/172917983)
    
    Signed-off-by: David Stevenson <stevensonda@vmware.com>

--- tasks/teardown.sh
+++ tasks/teardown.sh
@@ -11,4 +11,11 @@ mv director-state/.bosh $HOME/
 export bosh_cli=$(realpath bosh-cli/*bosh-cli-*)
 chmod +x $bosh_cli
 
+export BOSH_ENVIRONMENT=`$bosh_cli int director-creds.yml --path /internal_ip`
+export BOSH_CA_CERT=`$bosh_cli int director-creds.yml --path /director_ssl/ca`
+export BOSH_CLIENT=admin
+export BOSH_CLIENT_SECRET=`$bosh_cli int director-creds.yml --path /admin_password`
+
+$bosh_cli deployments --column name | xargs -n1 -I % "$bosh_cli" -n -d % delete-deployment
+$bosh_cli clean-up -n --all 
 $bosh_cli delete-env director.yml -l director-creds.yml
