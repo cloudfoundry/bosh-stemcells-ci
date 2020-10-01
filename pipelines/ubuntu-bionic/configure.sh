78bd0905f06473985f6471da734e6a5f0dd2f1c5
Ruben Koster <rkoster@starkandwayne.com>
2020-10-01 12:45:59 +0200

    Refactor stemcell pipelines to use common pipeline template [#174628522]

--- pipelines/ubuntu-bionic/configure.sh
+++ pipelines/ubuntu-bionic/configure.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -e
 
 until lpass status;do
   LPASS_DISABLE_PINENTRY=1 lpass ls a
+  sleep 1
 done
 
-until fly -t production status;do
-  fly -t production login
+until fly -t main status;do
+  fly -t main login
+  sleep 1
 done
 
-dir="$(dirname "$0")"
+pipeline_config=$(mktemp)
+ytt --dangerous-allow-all-symlink-destinations \
+    -f "$(dirname $0)" > $pipeline_config
 
-fly -t production set-pipeline \
-  -p "bosh:stemcells:ubuntu-bionic" \
-  -c "$dir/pipeline.yml" \
-  -l <(lpass show --notes "concourse:production pipeline:os-images") \
-  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells") \
-  -l <(lpass show --notes "bats-concourse-pool:vsphere secrets") \
-  -l <(lpass show --notes "stemcell-reminder-bot")
+name="$( basename $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
+fly -t main set-pipeline \
+  -p "bosh:stemcells:${name}" \
+  -l <(lpass show --notes "concourse:production pipeline:os-images" ) \
+  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
+  -l <(lpass show --notes "bosh-agent concourse secrets" ) \
+  -l <(lpass show --notes "bats-concourse-pool:vsphere secrets" ) \
+  -l <(lpass show --notes "bats-concourse-pool:vsphere nimbus secrets" ) \
+  -l <(lpass show --notes "stemcell-reminder-bot") \
+  -c "$pipeline_config"
