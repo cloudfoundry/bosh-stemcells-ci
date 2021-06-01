0ef3cc8ad788de8d2e2abcb8e61157b44973423c
ramonskie <ramonmakkelie@gmail.com>
2021-06-01 11:42:01 +0200

    change to gcp endpoints

--- pipelines/ubuntu-bionic/configure.sh
+++ pipelines/ubuntu-bionic/configure.sh
@@ -4,11 +4,6 @@ set -e
 
 FLY="${FLY_CLI:-fly}"
 
-until lpass status;do
-  LPASS_DISABLE_PINENTRY=1 lpass ls a
-  sleep 1
-done
-
 until "$FLY" -t "${CONCOURSE_TARGET:-bosh}" status;do
   "$FLY" -t "${CONCOURSE_TARGET:-bosh}" login
   sleep 1
@@ -20,5 +15,5 @@ ytt --dangerous-allow-all-symlink-destinations \
 
 name="$( basename $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
 "$FLY" -t "${CONCOURSE_TARGET:-bosh}" set-pipeline \
-  -p "bosh:stemcells:${name}" \
+  -p "stemcells-${name}" \
   -c "$pipeline_config"
