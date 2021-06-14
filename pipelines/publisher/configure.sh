e3b413a92ac3716e4208920a46faced6f51d5d72
ramonskie <ramonmakkelie@gmail.com>
2021-06-14 09:49:34 +0200

    publisher pipeline migration to gcp

--- pipelines/publisher/configure.sh
+++ pipelines/publisher/configure.sh
@@ -4,21 +4,14 @@ set -e
 
 FLY="${FLY_CLI:-fly}"
 
-until lpass status;do
-  LPASS_DISABLE_PINENTRY=1 lpass ls a
-  sleep 1
-done
-
-until "$FLY" -t "${CONCOURSE_TARGET:-main}" status;do
-  "$FLY" -t "${CONCOURSE_TARGET:-main}" login
+until "$FLY" -t "${CONCOURSE_TARGET:-bosh}" status;do
+  "$FLY" -t "${CONCOURSE_TARGET:-bosh}" login
   sleep 1
 done
 
 pipeline_config=$(mktemp)
 ytt -f "$(dirname $0)" > $pipeline_config
 
-"$FLY" -t "${CONCOURSE_TARGET:-main}" set-pipeline \
-  -p stemcells:publisher \
-  -c $pipeline_config \
-bosh int /tmp/used_variables.txt \
--l <(lpass show --notes "bionic_oss_stemcell_publisher.yml")
+"$FLY" -t "${CONCOURSE_TARGET:-bosh}" set-pipeline \
+  -p stemcells-publisher \
+  -c $pipeline_config
