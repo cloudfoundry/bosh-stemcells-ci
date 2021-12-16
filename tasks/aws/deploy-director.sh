66eecd0a99d4562292b3e64872660cd6d81d4272
ramonskie <ramonmakkelie@gmail.com>
2021-12-16 10:20:46 +0100

    add aws bats configs

---
+++ tasks/aws/deploy-director.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+source /etc/profile.d/chruby.sh
+chruby ruby
+
+cat > director-creds.yml <<EOF
+access_key_id:     ${AWS_ACCESS_KEY}
+secret_access_key: ${AWS_SECRET_KEY}
+private_key:       ${AWS_PRIVATE_KEY}
+EOF
+
+cat > director-vars.yml <<EOF
+region: ${AWS_REGION}
+az:     ${AWS_AZ}
+default_key_name: ${AWS_KEY_NAME}
+default_security_groups: [${AWS_SECURITY_GROUPS}]
+tags: [${TAG}]
+EOF
+
+cat > network-variables.yml <<EOF
+director_name: stemcell-smoke-tests-director
+subnet_id: ${AWS_SUBNET_ID}
+internal_ip: ${INTERNAL_IP}
+internal_cidr: ${INTERNAL_CIDR}
+internal_gw: ${INTERNAL_GW}
+reserved_range: [${RESERVED_RANGE}]
+EOF
+
+export bosh_cli=$(realpath bosh-cli/*bosh-cli-*)
+chmod +x $bosh_cli
+
+$bosh_cli interpolate bosh-deployment/bosh.yml \
+  -o bosh-deployment/aws/cpi.yml \
+  -o bosh-deployment/jumpbox-user.yml \
+  --vars-store director-creds.yml \
+  --vars-file director-vars.yml \
+  --vars-file network-variables.yml > director.yml
+
+set +e
+$bosh_cli create-env director.yml -l director-creds.yml
+deployed=$?
+cp -r $HOME/.bosh director-state/
+cp director.yml director-creds.yml director-state.json director-state/
+if [ $deployed -ne 0 ]
+then
+  exit 1
+fi
+set -e
+
+# occasionally we get a race where director process hasn't finished starting
+# before nginx is reachable causing "Cannot talk to director..." messages.
+sleep 10
+
+export BOSH_ENVIRONMENT=`$bosh_cli int director-creds.yml --path /internal_ip`
+export BOSH_CA_CERT=`$bosh_cli int director-creds.yml --path /director_ssl/ca`
+export BOSH_CLIENT=admin
+export BOSH_CLIENT_SECRET=`$bosh_cli int director-creds.yml --path /admin_password`
+
+$bosh_cli -n update-cloud-config bosh-deployment/aws/cloud-config.yml \
+          --ops-file bosh-stemcells-ci/ops-files/reserve-ips.yml \
+          --ops-file bosh-stemcells-ci/ops-files/disable-ephemeral-ip.yml \
+          --vars-file network-variables.yml \
+          --vars-file director-vars.yml
---
+++ tasks/bats/iaas/aws/prepare-bats-config.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+manifest_path() { bosh-cli int director-state/director.yml --path="$1" ; }
+creds_path() { bosh-cli int director-state/director-creds.yml --path="$1" ; }
+
+cat > bats-config/bats.env <<EOF
+export BOSH_ENVIRONMENT="$( manifest_path /instance_groups/name=bosh/networks/name=default/static_ips/0 2>/dev/null )"
+export BOSH_CLIENT="admin"
+export BOSH_CLIENT_SECRET="$( creds_path /admin_password )"
+export BOSH_CA_CERT="$( creds_path /director_ssl/ca )"
+export BOSH_GW_HOST="$( manifest_path /instance_groups/name=bosh/networks/name=default/static_ips/0 2>/dev/null )"
+export BOSH_GW_USER="jumpbox"
+export BAT_PRIVATE_KEY="$( creds_path /jumpbox_ssh/private_key )"
+
+export BAT_DNS_HOST="$( manifest_path /instance_groups/name=bosh/networks/name=default/static_ips/0 2>/dev/null )"
+
+export BAT_INFRASTRUCTURE=aws
+export BAT_NETWORKING=manual
+
+export BAT_RSPEC_FLAGS="--tag ~vip_networking --tag ~multiple_manual_networks --tag ~root_partition --tag ~raw_ephemeral_storage --tag ~skip_centos"
+EOF
+
+cat > interpolate.yml <<EOF
+---
+cpi: aws
+properties:
+  availability_zone: eu-central-1a
+  instances: 1
+  vip: ((VARS_VIP_DEFAULT)) # elastic ip for bat deployed VM
+  second_static_ip: ((STATIC_IP_DEFAULT)) # Secondary (private) IP to use for reconfiguring networks, must be in the primary network & different from static_ip
+  stemcell:
+    name: ((STEMCELL_NAME))
+    version: latest
+  networks:
+    - name: default
+      static_ip: ((STATIC_DEFAULT))
+      cidr: ((CIDR_DEFAULT))
+      reserved: [((RESERVED_DEFAULT))]
+      static: [((STATIC_DEFAULT))]
+      gateway: ((GATEWAY_DEFAULT))
+      subnet: ((SUBNETWORK_DEFAULT)) # VPC subnet
+      security_groups: ((SECURITY_GROUP)) # VPC security groups
+  key_name: ((KEY_NAME)) # (optional) SSH keypair name, overrides the director's default_key_name setting
+EOF
+
+bosh-cli interpolate \
+ --vars-env VARS \
+ interpolate.yml \
+ > bats-config/bats-config.yml
