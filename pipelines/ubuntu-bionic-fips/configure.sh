a503ce0a3b1ccc4c322fd75a65d50f657f30b25a
ramonskie <ramonmakkelie@gmail.com>
2022-02-23 16:00:21 +0100

    add bionic fips pipeline this is temporary as we should merge it into the version lines

---
+++ pipelines/ubuntu-bionic-fips/configure.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+FLY="${FLY_CLI:-fly}"
+
+until "$FLY" -t "${CONCOURSE_TARGET:-bosh-fips}" status;do
+  "$FLY" -t "${CONCOURSE_TARGET:-bosh-fips}" login
+  sleep 1
+done
+
+pipeline_config=$(mktemp)
+ytt --dangerous-allow-all-symlink-destinations \
+    -f "$(dirname $0)" > $pipeline_config
+
+name="$( basename $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
+"$FLY" -t "${CONCOURSE_TARGET:-bosh-fips}" set-pipeline \
+  -p "stemcells-${name}" \
+  -c "$pipeline_config"
