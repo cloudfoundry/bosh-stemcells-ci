1b0756bff58b3654dc33ac8e9d4f36442539dc98
ramonskie <ramonmakkelie@gmail.com>
2022-07-14 10:16:37 +0200

    tag published stemcell

---
+++ tasks/light-aws/tag-aws-ami-light.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+cd candidate-aws-light-stemcell
+tar -xzf *.tgz stemcell.MF
+OS=$( cat stemcell.MF | grep operating_system | cut -f2 -d: | tr -d ' ')
+#IGNORE gov and china stemcells. these are in a different account
+AMI_LIST=$(cat stemcell.MF | grep ami- | tr -d ' '| grep -v 'gov-\|cn-')
+VERSION=$(cat stemcell.MF | grep "^version" | cut -f2 -d: | tr -d ' ' | tr -d "'")
+for AMI_LINE in $AMI_LIST; do
+  AMI_REGION=$(echo $AMI_LINE | cut -f1 -d:)
+  AMI_ID=$(echo $AMI_LINE | cut -f2 -d:)
+
+  TAGS="[
+    { \"Key\": \"published\", \"Value\": \"true\" },
+    { \"Key\": \"Name\"     , \"Value\": \"$OS-$VERSION\" },
+    { \"Key\": \"Distro\"   , \"Value\": \"$OS\" },
+    { \"Key\": \"Version\"  , \"Value\": \"$VERSION\" }
+  ]"
+
+  aws ec2 create-tags --resources "$AMI_ID" --region "$AMI_REGION" --tags "$TAGS"
+done
