471b331fb966af0215e7dcb3120cce322ddb5af3
Daniel Felipe Ochoa <danielfelipo@vmware.com>
2022-08-31 11:22:19 -0700

    bump-bosh-blobstore-cli now sets a URL file
    This allows simimplificaiton of the stemcell builder scripts
    
    Signed-off-by: aram price <pricear@vmware.com>

--- tasks/bump-bosh-blobstore-cli.sh
+++ tasks/bump-bosh-blobstore-cli.sh
@@ -4,19 +4,22 @@ set -x
 
 git clone bosh-linux-stemcell-builder bosh-linux-stemcell-builder-out
 
-version=$( cat bosh-blobstore-cli/version )
-sha256sum=$( sha256sum -b bosh-blobstore-cli/*cli* | awk '{print $1}' )
+url=$(cat bosh-blobstore-cli/url)
+version=$(cat bosh-blobstore-cli/version)
+sha256sum=$(sha256sum -b bosh-blobstore-cli/*cli* | awk '{print $1}')
 
+echo "${url}" > \
+  "bosh-linux-stemcell-builder-out/stemcell_builder/stages/blobstore_clis/assets/bosh-blobstore-${BLOBSTORE_TYPE}.url"
 echo "${version}" > \
   "bosh-linux-stemcell-builder-out/stemcell_builder/stages/blobstore_clis/assets/bosh-blobstore-${BLOBSTORE_TYPE}.version"
 echo "${sha256sum}" > \
   "bosh-linux-stemcell-builder-out/stemcell_builder/stages/blobstore_clis/assets/bosh-blobstore-${BLOBSTORE_TYPE}.sha256sum"
 
 pushd bosh-linux-stemcell-builder-out
-    if [ "$(git status --porcelain)" != "" ]; then
-        git add -A
-        git config --global user.email "ci@localhost"
-        git config --global user.name "CI Bot"
-        git commit -m "bump bosh-blobstore-${BLOBSTORE_TYPE}/${version}"
-    fi
+  if [ "$(git status --porcelain)" != "" ]; then
+    git add -A
+    git config --global user.email "ci@localhost"
+    git config --global user.name "CI Bot"
+    git commit -m "bump bosh-blobstore-${BLOBSTORE_TYPE}/${version}"
+  fi
 popd
