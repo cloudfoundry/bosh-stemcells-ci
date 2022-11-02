90ae7bb349156a99006e69620a59885bf5bb76ae
Daniel Felipe Ochoa <danielfelipo@vmware.com>
2022-11-02 17:47:32 -0500

    Assets directory is created before use.
    Signed-off-by: Aram Price <pricear@vmware.com>

--- tasks/bump-open-vm-tools.sh
+++ tasks/bump-open-vm-tools.sh
@@ -10,6 +10,8 @@ pushd open-vm-tools
   sha256sum=$(sha256sum -b open-vm-tools-*.tar.gz | awk '{print $1}')
 popd
 
+mkdir -p "bosh-linux-stemcell-builder-out/stemcell_builder/stages/system_open_vm_tools/assets/"
+
 echo "${url}" > \
   "bosh-linux-stemcell-builder-out/stemcell_builder/stages/system_open_vm_tools/assets/open-vm-tools.url"
 echo "${version}" > \
