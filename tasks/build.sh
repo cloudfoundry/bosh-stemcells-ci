3a04ea098ca96318e06ca9780f6ca8cc0d57c1e2
Long Nguyen <nguyenlo@vmware.com>
2023-01-11 07:17:39 -0500

    Fix build.sh to support xenial

--- tasks/build.sh
+++ tasks/build.sh
@@ -70,15 +70,16 @@ sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
   set -e
 
   cd bosh-linux-stemcell-builder
-  bundle update --bundler
 case $OS_NAME
 in
 # Because of the difference in build environments between Xenial and other Ubuntu stemcell lines (currently only Jammy)
 # we must run 'bundle install' as the root user for it to function correctly.
 "xenial")
+    sudo bundle update --bundler
     sudo bundle install --local
     ;;
 *)
+    bundle update --bundler
     bundle install --local
     ;;
 esac
