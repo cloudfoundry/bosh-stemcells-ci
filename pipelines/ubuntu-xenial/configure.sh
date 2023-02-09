6c90c773e8743f7cb86b8011a3b4500aca571616
Joseph Palermo <jpalermo@pivotal.io>
2023-02-09 10:33:27 -0800

    Remove LastPass entry

--- pipelines/ubuntu-xenial/configure.sh
+++ pipelines/ubuntu-xenial/configure.sh
@@ -20,7 +20,8 @@ ytt -f "$(dirname $0)" > $pipeline_config
 
 "$FLY" -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline \
   -p "stemcells-ubuntu-xenial" \
-  -l <(lpass show --notes "stemcell-reminder-bot") \
   -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells lts") \
+  -v story_creator_tracker_project_id=2484143 \
+  -v story_creator_private_tracker_project_id=2484143 \
   -c "$pipeline_config"
 
