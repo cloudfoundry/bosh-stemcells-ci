fe8111fdab7e0802dc101025a46ba5df5f5086c1
aram price <pricear@vmware.com>
2023-11-08 14:00:03 -0800

    Use `go run` instead of expecting `ginkgo` on the path

--- tasks/light-aws/test-unit.sh
+++ tasks/light-aws/test-unit.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
-set -e
+set -euo pipefail
 
 echo "Running unit tests"
 
 pushd builder-src > /dev/null
-  ginkgo -p -r -skipPackage "driver,integration"
-  ginkgo -p -r driverset # driverset is skipped by previous command
+  go run github.com/onsi/ginkgo/v2/ginkgo -p -r --skip-package "driver,integration"
+  go run github.com/onsi/ginkgo/v2/ginkgo -p -r driverset # driverset is skipped by previous command
 popd
