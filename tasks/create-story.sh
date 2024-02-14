3c2cae006768e1802c60dbf0a5b2ecba63ccfc7c
Chris Selzo <chris.selzo@broadcom.com>
2024-02-13 16:46:51 -0800

    Update stemcell story creation to add a task when story is security related
    This is so we know to validate the USN that triggererd the story creation
    is applicable to the stemcell
    
    Signed-off-by: Joseph Palermo <joseph.palermo@broadcom.com>

--- tasks/create-story.sh
+++ tasks/create-story.sh
@@ -7,8 +7,12 @@ set -euo pipefail
 : ${PROJECT_ID:?}
 : ${DESCRIPTION:?}
 
-FILTER_UNSTARTED=$(jq -rn '"-type:release state:unstarted" | @uri')
+tasks="[]"
+if [[ ${DESCRIPTION} == *"security"* ]]; then
+  tasks='[ { "description": "Validate the packages for the USN referenced in #bosh-private CFF Slack are included in the Stemcell" } ]'
+fi
 
+FILTER_UNSTARTED=$(jq -rn '"-type:release state:unstarted" | @uri')
 UNSTARTED_STORIES=$(curl -s -X GET -H "X-TrackerToken: $TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories?filter=$FILTER_UNSTARTED")
 FIRST_UNSTARTED_STORY=$(echo "$UNSTARTED_STORIES" | jq '.[0].id')
 
@@ -23,6 +27,7 @@ curl --show-error --fail -X POST "https://www.pivotaltracker.com/services/v5/pro
   "description" : "It's time to build a new version of stemcells which include the latest upstream vulnerability fixes. The pipeline should have automatically triggered when this story was created to kick off that process. Detailed instructions on what to do to make a patch lives [here](https://github.com/pivotal-cf/bosh-team/blob/master/stemcells/flow-of-stemcell-pipelines.md#cutting-a-new-stemcell-patch)\\n\\nEnsure the OS image and subsequent stemcell builds finished successfully.\\n\\n**Acceptance Criteria**\\n\\n* A new version of the stemcell can be published from the pipeline.",
   "story_type" : "feature",
   "labels" : ["$BRANCH", "stemcell", "sec"],
-  "before_id" : $FIRST_UNSTARTED_STORY
+  "before_id" : $FIRST_UNSTARTED_STORY,
+  "tasks" : ${tasks}
 }
 EOF
