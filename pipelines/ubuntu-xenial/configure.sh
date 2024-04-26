4d22e66480746e6b181db7583eea766647d164e3
Chris Selzo <chris.selzo@broadcom.com>
2024-04-26 12:06:30 -0700

    CI: Replace lpass note with credhub references

--- pipelines/ubuntu-xenial/configure.sh
+++ pipelines/ubuntu-xenial/configure.sh
@@ -4,11 +4,6 @@ set -e
 
 FLY="${FLY_CLI:-fly}"
 
-until lpass status;do
-  LPASS_DISABLE_PINENTRY=1 lpass ls a
-  sleep 1
-done
-
 until "$FLY" -t "${CONCOURSE_TARGET:-bosh-ecosystem}" status;do
   "$FLY" -t "${CONCOURSE_TARGET:-bosh-ecosystem}" login
   sleep 1
@@ -20,7 +15,6 @@ ytt -f "$(dirname $0)" > $pipeline_config
 
 "$FLY" -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline \
   -p "stemcells-ubuntu-xenial" \
-  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells lts") \
   -v story_creator_tracker_project_id=2484143 \
   -v story_creator_private_tracker_project_id=2484143 \
   -c "$pipeline_config"
