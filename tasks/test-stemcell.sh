35e3a8598a3d52730893a59299675740b4bcc64b
aram price <aram.price@broadcom.com>
2024-07-26 14:49:13 -0700

    bash fixups

--- tasks/test-stemcell.sh
+++ tasks/test-stemcell.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
+set -e
 
-set -eu
+BOSH_BINARY_PATH=$(realpath bosh-cli/*bosh-cli-*)
+chmod +x "${BOSH_BINARY_PATH}"
 
-export BOSH_BINARY_PATH=$(realpath bosh-cli/*bosh-cli-*)
-chmod +x $BOSH_BINARY_PATH
+BOSH_CA_CERT="$(${BOSH_BINARY_PATH} int director-state/director-creds.yml --path /director_ssl/ca)"
+BOSH_CLIENT_SECRET="$(${BOSH_BINARY_PATH} int director-state/director-creds.yml --path /admin_password)"
+BOSH_ENVIRONMENT="$(${BOSH_BINARY_PATH} int director-state/director-creds.yml --path /internal_ip)"
+SYSLOG_RELEASE_PATH="$(realpath syslog-release/*.tgz)"
+OS_CONF_RELEASE_PATH="$(realpath os-conf-release/*.tgz)"
+STEMCELL_PATH="$(realpath stemcell/*.tgz)"
+BOSH_stemcell_version="$(realpath stemcell/.resource/version | xargs -n 1 cat)"
 
-export BOSH_ENVIRONMENT=`$BOSH_BINARY_PATH int director-state/director-creds.yml --path /internal_ip`
-export BOSH_CA_CERT=`$BOSH_BINARY_PATH int director-state/director-creds.yml --path /director_ssl/ca`
-export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`$BOSH_BINARY_PATH int director-state/director-creds.yml --path /admin_password`
-export SYSLOG_RELEASE_PATH=$(realpath syslog-release/*.tgz)
-export OS_CONF_RELEASE_PATH=$(realpath os-conf-release/*.tgz)
-export STEMCELL_PATH=$(realpath stemcell/*.tgz)
-export BOSH_stemcell_version=\"$(realpath stemcell/.resource/version | xargs -n 1 cat)\"
+export BOSH_BINARY_PATH
+export BOSH_CA_CERT
+export BOSH_CLIENT="admin"
+export BOSH_CLIENT_SECRET
+export BOSH_ENVIRONMENT
+export SYSLOG_RELEASE_PATH
+export OS_CONF_RELEASE_PATH
+export STEMCELL_PATH
+export BOSH_stemcell_version
 
-if $BOSH_BINARY_PATH int director-state/director-creds.yml --path /jumpbox_ssh > /dev/null 2>&1 ; then
-  jumpbox_private_key=$(mktemp)
-  $BOSH_BINARY_PATH int director-state/director-creds.yml --path /jumpbox_ssh/private_key > ${jumpbox_private_key}
-  chmod 0600 ${jumpbox_private_key}
-  export BOSH_GW_PRIVATE_KEY=${jumpbox_private_key}
-  export BOSH_GW_USER=jumpbox
-  export BOSH_GW_HOST=$BOSH_ENVIRONMENT
+if ${BOSH_BINARY_PATH} int director-state/director-creds.yml --path /jumpbox_ssh > /dev/null 2>&1 ; then
+  jumpbox_private_key="$(mktemp)"
+  ${BOSH_BINARY_PATH} int director-state/director-creds.yml --path /jumpbox_ssh/private_key > "${jumpbox_private_key}"
+  chmod 0600 "${jumpbox_private_key}"
+  export BOSH_GW_PRIVATE_KEY="${jumpbox_private_key}"
+  export BOSH_GW_USER="jumpbox"
+  export BOSH_GW_HOST="${BOSH_ENVIRONMENT}"
 fi
 
 pushd bosh-linux-stemcell-builder
