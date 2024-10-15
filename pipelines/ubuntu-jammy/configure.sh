055d3a830c0cc8b369b660ca34626a6cc39adc2d
Nitin Ravindran <nitin.ravindran@broadcom.com>
2024-10-15 12:10:40 -0700

    Remove "github story creation" tasks
    Signed-off-by: aram price <aram.price@broadcom.com>

--- pipelines/ubuntu-jammy/configure.sh
+++ pipelines/ubuntu-jammy/configure.sh
@@ -4,17 +4,17 @@ set -e
 
 FLY="${FLY_CLI:-fly}"
 
-until "$FLY" -t "${CONCOURSE_TARGET:-stemcell}" status;do
-  "$FLY" -t "${CONCOURSE_TARGET:-stemcell}" login
+until "${FLY}" -t "${CONCOURSE_TARGET:-stemcell}" status;do
+  "${FLY}" -t "${CONCOURSE_TARGET:-stemcell}" login
   sleep 1
 done
 
 pipeline_config=$(mktemp)
 ytt --dangerous-allow-all-symlink-destinations \
-    -f "$(dirname $0)" > $pipeline_config
+    -f "$(dirname "${0}")" > "${pipeline_config}"
 
-name="$( basename $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
-"$FLY" -t "${CONCOURSE_TARGET:-stemcell}" set-pipeline \
+name="$( basename "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
+"${FLY}" -t "${CONCOURSE_TARGET:-stemcell}" set-pipeline \
   --team=stemcell \
   -p "stemcells-${name}" \
-  -c "$pipeline_config"
+  -c "${pipeline_config}"
