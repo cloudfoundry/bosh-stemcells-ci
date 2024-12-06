6f66d620dff04baa6d82dd479b252e539b1cf262
Rajath Agasthya <rajath.agasthya@broadcom.com>
2024-12-06 16:49:26 -0600

    Add package filtering before combining new USNs into the usn-log file
    - Extracted a usn-shared-functions to be used by the new check-if-usn-is-applicable as well as continue to be used by check-usn-package
    - We unfortunately have to use a try in the pipeline to simulate if/else to prevent pipelines from triggering when the USN does not apply
    - Changed the process-high-critical-cves job to get the CVE resource with "version: every". This probably was leading to USNs missing from the usn-log if more than one USN was published in a small window
    - Also cleaned up the bump-message script which once-upon-a-time included details on the USN triggering the pipeline, but has not for some time.
        - If somebody wants to trace which USN triggered a pipeline, it is fairly easy to do by checking the pipeline-trigger resource version and seeing which job generated it
    
    Signed-off-by: Joseph Palermo <joseph.palermo@broadcom.com>

---
+++ tasks/usn-processing/usn-shared-functions.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+#
+# This script goes through the list of ubuntu security notices (USNs) and makes sure that packages
+# listed in every USN are available in apt repositories for installation.
+# We do this to make sure that a newly released stemcell includes fixes for USNs that tirggered the build.
+#
+
+function process_packages {
+  local package_version_for_usn=("$@")
+
+  for package_version in "${package_version_for_usn[@]}"
+  do
+    # single package
+    package=$(echo "$package_version" | cut -d ':' -f1)
+    version=$(echo "$package_version" | cut -d ':' -f2)
+
+    if is_package_installed "$package";
+    then
+      PACKAGE_INCLUDED_IN_STEMCELL=true
+      echo "checking: $package"
+      check_package "$package" "$version"
+    else
+      echo "skip: $package"
+    fi
+  done
+}
+
+function candidate_version_to_install {
+  local package_name=$1
+  local candidate
+  candidate=$(sudo apt policy "$package_name" 2>/dev/null | grep Candidate | tr -s ' ' | cut -d ' ' -f3)
+  echo "$candidate"
+}
+
+function check_package {
+  local package=$1
+  local version=$2
+  local candidate_version
+
+  candidate_version=$(candidate_version_to_install "$package")
+
+  if ! dpkg --compare-versions "$candidate_version" ge "$version";
+  then
+    ALL_PACKAGE_VERSIONS_AVAILABLE=false
+    echo -e "L \e[31mexpected version from USN: $version, available in repo: $candidate_version\e[0m"
+  else
+    echo -e "L repo version: $candidate_version matches USN version: $version"
+  fi
+}
+
+function enable_esm {
+  apt-get update
+  apt-get install -y ca-certificates ubuntu-advantage-tools
+  ua attach "${ESM_TOKEN}" --no-auto-enable
+  ua enable esm-infra
+}
+
+function process_usns {
+  local usn_log_json=$1
+
+  mapfile -t usn_urls < <(jq -r '.url | select(.|test("USN"))' "$usn_log_json" | sort | uniq)
+
+  for usn_url in "${usn_urls[@]}"
+  do
+    # list of packages
+    echo -e "\n>>>>> $usn_url <<<<<"
+
+    mapfile -t package_version_for_usn < <( curl -s "$usn_url.json" | jq --arg os $OS -r '.release_packages[$os][] | select(.is_source==false) | "\(.name):\(.version)"')
+    process_packages "${package_version_for_usn[@]}" "$usn_url"
+  done
+}
+
+function is_package_installed {
+  local package=$1
+  echo "$INSTALLED_PACKAGES" | grep -q "$package$"
+  return $?
+}
+
+if [[ -n "$ESM_TOKEN" ]]; then
+  enable_esm
+fi
+
+if [[ -z "${OS}" ]]; then
+  echo "Environment variable 'OS' must be set, and not empty"
+  exit 1
+fi
+
+# Depends on apt package list being up-to-date, make sure apt-get update is run before this is executed
+sudo apt-get update
+INSTALLED_PACKAGES=$(cat bosh-linux-stemcell-builder/bosh-stemcell/spec/assets/dpkg-list-ubuntu-${OS}*.txt | sort | uniq | sed -e 's/:amd64//g')
+ALL_PACKAGE_VERSIONS_AVAILABLE=true
+PACKAGE_INCLUDED_IN_STEMCELL=false
