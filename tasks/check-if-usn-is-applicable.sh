305f1330d773517bfae33f025f9e068ecf6c68ef
Ming Xiao <ming.xiao@broadcom.com>
2024-12-17 11:11:23 -0800

    fix `log-low-mediums-cve` task
    change the name of the output of the `check-if-usn-is-applicable` job so that
    it does not conflict with the `usn-log` resource

--- tasks/check-if-usn-is-applicable.sh
+++ tasks/check-if-usn-is-applicable.sh
@@ -11,12 +11,11 @@ process_usns "${usn_json}"
 
 if [ "$PACKAGE_INCLUDED_IN_STEMCELL" == true ]
 then
-  cp usn-log/usn-log.json existing-usn-log.json
-  jq -s --slurpfile new_usn ${usn_json} '. + $new_usn | unique_by(.url) | .[]' > usn-log/usn-log.json < existing-usn-log.json
-  echo "true" > usn-log/success
+  jq -s --slurpfile new_usn ${usn_json} '. + $new_usn | unique_by(.url) | .[]' > updated-usn-log/usn-log.json < usn-log/usn-log.json
+  echo "true" > updated-usn-log/success
   exit 0
 else
-  echo "true" > usn-log/success
+  echo "true" > updated-usn-log/success
   echo "Packages for USN are not included in stemcell"
   exit 1
 fi
