306307aa1b524d65a5d106165b7d2fee5945c5e9
aram price <aram.price@broadcom.com>
2026-03-23 15:45:21 +0100

    Remove ununtu-xenial
    Also remove tasks which are now unused.

--- tasks/write-usn-message.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-usn_json="${PWD}/usn-source/usn.json"
-mkdir -p usn-log-in
-touch usn-log-in/usn-log.json
-
-jq -s --slurpfile new_usn $usn_json '. + $new_usn | unique_by(.url) | .[]' > usn-log-out/usn-log.json < usn-log-in/usn-log.json
